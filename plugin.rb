880076c9c9b2fe27463ebc8b2d6d0bf1938e6244
Miles Davis <mrdavis1997gmail.com>
2021-03-20 17:00:00 -0500

    added after_initialize do. Might be the issue.

--- plugin.rb
+++ plugin.rb
@@ -4,6 +4,8 @@
 # authors: Miles Davis
 # url: https://github.com/MrDavis97/discourse-math-by-miles
 
+after_initialize do
+
 LANDING_HOME ||= "/welcome"
 
   add_model_callback(:application_controller, :before_action) do
